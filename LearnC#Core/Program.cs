8cb2bdb2e262d2f01bda633d71b542ef3fafce09
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2024-11-14 23:45:45 +0700

    Add project files.

---
+++ LearnC#Core/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhập vào chiều dài của đường tròn:");
+        double radius = double.Parse(Console.ReadLine());
+        double area = Math.PI * radius * radius;
+        double circumference = 2 * Math.PI * radius;
+        Console.WriteLine("Bán kính hình tròn:" + radius);
+        Console.WriteLine("Diện tích hình tròn:" + circumference);
+    }
+}
